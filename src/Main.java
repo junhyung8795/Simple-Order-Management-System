2192e7acd3fcab0becf1e6332e687f1b14fd7a4f
junhyung8795 <junhyung8795@naver.com>
2025-04-11 15:41:58 +0900

    structure planning

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //Member는 Order와 여태 주문한 상품 목록을 가지고 있고, Order안에 결제 상품정보, 결제 상태, 결제 정보를 넣는 Payment
+        //Payment 안에는 결제 수단(국민카드 Or 카카오페이)
+        //상품정보에는 상품명과 상품 가격이 들어간다.
 
-        }
+    }
 }
